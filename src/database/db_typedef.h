34499381f6b4a96403b033c0b95a8eae4c796257
Reyad Attiyat <reyad.attiyat@gmail.com>
2013-02-08 12:59:59 -0600

    still fixing typedef promblems

---
+++ src/database/db_typedef.h
@@ -0,0 +1,20 @@
+/*
+ * db_typedef.h
+ *
+ *  Created on: Feb 8, 2013
+ *      Author: reyad
+ */
+
+#ifndef DB_TYPEDEF_H_
+#define DB_TYPEDEF_H_
+
+typedef struct table_ table_t;
+typedef	struct column_table_ column_table_t;
+typedef struct query_ query_t;
+typedef struct db_config_ db_config;
+typedef struct row_ row_t;
+typedef struct results_table_ results_table_t;
+
+typedef struct query_parameters_ query_parameters_t;
+
+#endif /* DB_TYPEDEF_H_ */
