bd27f62178d84ccad93fa22ca65ac953c416e0ec
Reyad Attiyat <reyad.attiyat@gmail.com>
2014-08-14 02:20:17 -0500

    Cleanup old xml code moved shared memory code
    Remove all old tree based xml code as this will be
    replaced by xpath. Move setup_shared_memory to
    util c file and library to assit in testing.

--- src/indexers/indexer.c
+++ src/indexers/indexer.c
@@ -3,50 +3,25 @@
 #include "database/db_manager.h"
 #include "database/db_object.h"
 
+static const char* INDEXER_ERROR_HEADER = "Indexer Error";
+
 static int indexer_read_xml_config(indexer_t* indexer, db_manager_t* db_manager, const char* xml_config_file_path){
 	error_messages_t* error_messages = indexer->error_messages;
-	apr_xml_parser* xml_parser;
-	apr_xml_doc* xml_doc;
-	apr_xml_elem* xml_elem;
 
 	apr_file_t* xml_file;
 
 	apr_status_t status;
 
 	apr_pool_t* pool = indexer->pool;
-	apr_size_t max_element_size = 255;
 
 	indexer->db_objects = apr_array_make(pool, 16, sizeof(db_object_t));
 	
 	status = apr_file_open(&xml_file, xml_config_file_path,APR_READ,APR_OS_DEFAULT ,pool);
-	if(status != APR_SUCCESS){
-		 	error_messages_add(error_messages,ERROR,"Error opening Database XML file","Check file permission or path");
-		 	return -1;
-	}
-	xml_parser = apr_xml_parser_create(pool);
-
-	status = apr_xml_parse_file(pool, &xml_parser,&xml_doc,xml_file,20480);
-	if(status != APR_SUCCESS){
-		 error_messages_add(error_messages,ERROR,"Error parsing Database XML file","Verify the XML syntax.");
-		 return -1;
+	if (status != APR_SUCCESS) {
+		error_messages_addf(error_messages, ERROR, INDEXER_ERROR_HEADER, "Check file permission or path (%s)", xml_config_file_path);
+		return -1;
 	}
 
-	if(apr_strnatcmp(xml_doc->root->name, "indexer") != 0){
-		 error_messages_add(error_messages,ERROR,"Invalid XML FILE","No element 'app' found as root.");
-		 return -1;
-	}
-
-	for(xml_elem = xml_doc->root->first_child;xml_elem != NULL;xml_elem = xml_elem->next){
-		//db_params_t* db_params;
-
-		//Handle a database/queries for application
-		if(apr_strnatcmp(xml_elem->name,"database") ==0){
-			//get_xml_attr(pool,xml_elem, "db_id", &db_id);
-		//Handle Friendly name for application
-		}else if(apr_strnatcmp(xml_elem->name,"fname") ==0){
-			apr_xml_to_text(pool,xml_elem,APR_XML_X2T_INNER,NULL,NULL,&(indexer->friendly_name),&max_element_size);
-		}
-	}
 
 	apr_file_close(xml_file);
 	return 0;
--- src/mod_mp.c
+++ src/mod_mp.c
@@ -38,12 +38,8 @@
 #include <util_filter.h>
 #include <http_log.h>
 #include "unixd.h"
-
-#include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
 #include <time.h>
 
 #include "indexers/indexer.h"
@@ -92,55 +88,6 @@ static const char* mp_set_indexers_xml_config_dir (cmd_parms* cmd, void* cfg, co
 	return NULL;
 }
 
-int setup_shared_memory(apr_shm_t** shm,apr_size_t size,const char* file_path, apr_pool_t* pool){
-	apr_status_t rv;
-    apr_uid_t uid = 0;
-    apr_gid_t gid = 0;
-    key_t shm_key;
-    int shm_id;
-    struct shmid_ds buf;
-    time_t t;
-
-    int status;
-
-
-    //apr_uid_get(&uid,&gid,"apache", pool);
-
-	rv = apr_shm_create(shm, size,file_path , pool);
-	if(rv == 17){
-		//It already exsits lets kill it with fire
-		rv = apr_shm_attach(shm,file_path , pool);
-		rv = apr_shm_destroy(*shm);
-		rv = apr_shm_create(shm, size,file_path , pool);
-	}
-	if(rv != APR_SUCCESS){
-
-		*shm = NULL;
-		return -1;
-	}
-
-	shm_key = ftok(file_path,1);
-	shm_id = shmget(shm_key,size,IPC_CREAT | 0666);
-
-	status = shmctl(shm_id, IPC_STAT, &buf);
-	if(status != 0){
-		return -2;
-	}
-	time(&t);
-	buf.shm_ctime = t;
-	buf.shm_perm.gid = ap_unixd_config.group_id;
-	buf.shm_perm.uid = ap_unixd_config.user_id;
-	buf.shm_perm.mode = 0666;
-	status = shmctl(shm_id, IPC_SET, &buf);
-	if(status != 0){
-		return -3;
-	}
-
-	status = chmod(file_path, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	status = chown(file_path,uid,gid);
-
-	return 0;
-}
 
 
 
